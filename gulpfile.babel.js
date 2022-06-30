6791d9a374610499113e504309dc7de3506fe7de
sung-da <desung2020@naver.com>
2022-06-30 13:39:58 +0900

    #4 SCSS COMPILATION

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,27 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import autoPrefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+
+const sass = require("gulp-sass")(require("node-sass"));
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
   },
 };
 
@@ -14,8 +30,30 @@ const pug = () =>
 
 const clean = () => del(["build"]);
 
-const prepare = gulp.series([clean]);
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const assets = gulp.series([pug]);
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
-export const dev = gulp.series([prepare, assets]);
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoPrefixer())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([pug, styles]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
